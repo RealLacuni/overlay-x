d722ae58cdf78e862247ebecb1363e27e3240bb3
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2023-10-30 00:37:18 -0400

    Created Toggle component for inverse

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Switch } from '@headlessui/react';
+import { classNames } from '../util/classNames';
+
+type ToggleProps = {
+    checked: boolean;
+    handleChange: (val : boolean) => void;
+}
+const Toggle = ({checked, handleChange} : ToggleProps ) => {
+  const [enabled, setEnabled] = useState(checked);
+  return (
+    <Switch
+      checked={enabled}
+      onChange={() => {
+        setEnabled(!enabled);
+        handleChange;
+      }}
+      className={classNames(
+        enabled ? 'bg-indigo-600' : 'bg-gray-200',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+      )}
+    >
+      <span className="sr-only">Use setting</span>
+      <span
+        aria-hidden="true"
+        className={classNames(
+          enabled ? 'translate-x-5' : 'translate-x-0',
+          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+        )}
+      />
+    </Switch>
+  );
+};
+
+export default Toggle;
