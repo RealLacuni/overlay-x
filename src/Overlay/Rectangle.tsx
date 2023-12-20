0c59e5e968fd9a6f4cdee4c2cc2567dbd843c6e8
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2023-12-20 02:01:45 -0500

    rectangle fix,
    dimension / 3 is the way

--- src/Overlay/Rectangle.tsx
+++ src/Overlay/Rectangle.tsx
@@ -13,12 +13,12 @@ type RectangleProps = {
 const Rectangle = ({ profile, cursorPosition, renderMiniature }: RectangleProps) => {
   const { color, opacity } = profile;
   let offset = profile.offset;
-  let width = profile.width;
-  let height = profile.height;
+  let width = profile.width == 100 ? 2000 : 8*profile.width;
+  let height = profile.height == 100 ? 2000 : 8*profile.height;
 
-  const scale = 0.5;
+  const scale = 0.12;
   if (renderMiniature) {
-    offset *= scale;
+    offset *= 2*scale;
     width *= scale;
     height *= scale;
   }
@@ -31,20 +31,19 @@ const Rectangle = ({ profile, cursorPosition, renderMiniature }: RectangleProps)
           : 'fixed top-0 left-0 w-full h-full'
       } bg-[${color}] pointer-events-none`}
     >
-      {' '}
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" style={{ opacity: opacity / 100 }}>
         {!profile.inverse ? (
           <rect
-            x={renderMiniature ? `${50 - width / 2}%` : cursorPosition.x - width / 2}
-            y={renderMiniature ? `${50 - height / 2}%` : cursorPosition.y - height / 2}
+            x={renderMiniature ? `${50 - width / 3}%` : cursorPosition.x - width / 2}
+            y={renderMiniature ? `${50 - height / 3}%` : cursorPosition.y - height / 2}
             width={width}
             height={height}
             fill={color}
           />
         ) : (
           <rect
-            x={renderMiniature ? `${50 - width / 2 - offset/3}%` : cursorPosition.x - width / 2 - offset / 2}
-            y={renderMiniature ? `${50 - height / 2 - offset/3}%` : cursorPosition.y - height / 2 - offset / 2}
+            x={renderMiniature ? `${50 - width / 3 - offset / 3}%` : cursorPosition.x - width / 2 - offset / 2}
+            y={renderMiniature ? `${50 - height / 3 - offset / 3}%` : cursorPosition.y - height / 2 - offset / 2}
             width={width + offset}
             height={height + offset}
             fill="transparent"
--- src/components/ShapePreview.tsx
+++ src/components/ShapePreview.tsx
@@ -25,7 +25,7 @@ const ShapePreview = () => {
       <div className="absolute bottom-0 right-4 w-36 h-36 rounded-lg">
         <div
           className="absolute bg-transparent"
-          style={{ left: '51%', top: '51%', transform: 'translate(-50%, -50%)' }}
+          style={{ left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}
         >
           <svg fill="#000000" width="10px" height="10px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M20.978 13.21a1 1 0 0 0-.396-1.024l-14-10a.999.999 0 0 0-1.575.931l2 17a1 1 0 0 0 1.767.516l3.612-4.416 3.377 5.46 1.701-1.052-3.357-5.428 6.089-1.218a.995.995 0 0 0 .782-.769z" />
