de0d625ed2cec291f11bc404c59b5bb6c0b7ab71
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2023-12-31 18:05:08 -0500

    Update AboutPage and Home components

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,26 +10,17 @@ const sendLoadOverlay = (useDev = false) => {
 const Home = () => {
   const { preferences } = useContext(PreferenceContext);
   const nav = useNavigate();
-  
 
   return (
     <>
-      <div className={'flex flex-col h-screen justify-start pt-20 bg-slate-50 items-center gap-12 text-blue-900'}>
-        <SecondaryButton className={' hover:text-black'} onClick={() => sendLoadOverlay(false)}>
+      <div className={'flex flex-col h-screen justify-center gap-12 bg-slate-50 items-center text-blue-900'}>
+        <SecondaryButton className={' hover:text-black w-28'} onClick={() => sendLoadOverlay(false)}>
           Launch Overlay
         </SecondaryButton>
-        <SecondaryButton className={' hover:text-black'} onClick={() => {nav('/settings')}}>
+        <SecondaryButton className={' hover:text-black w-28'} onClick={() => {nav('/settings')}}>
           Settings
         </SecondaryButton>
-        <SecondaryButton
-          className={' hover:text-black'}
-          onClick={() => {
-            nav('/about');
-          }}
-        >
-          About
-        </SecondaryButton>
-        <SecondaryButton className={' hover:text-black bg-indigo-600 text-indigo-50 mt-10'} onClick={window.Main.Close}>
+        <SecondaryButton className={' hover:text-black bg-indigo-600 text-indigo-50 w-32'} onClick={window.Main.Close}>
           Close
         </SecondaryButton>
 
@@ -39,6 +30,11 @@ const Home = () => {
         </p>
       </div>
       {/* absolute div containing the current version */}
+      <div className='absolute bottom-0 left-0 px-1'>
+        <a href='/about' className=' text-gray-600 underline text-sm'>
+          About</a>
+
+      </div>
       <div className={'absolute bottom-0 right-0 p-1 text-xs text-gray-500'}>
         <div className={'flex flex-row align-text-bottom gap-1 items-end'}>
           <div className = {'hover:cursor-pointer'}onClick={() => window.Main.OpenLink('https://github.com/RealLacuni/overlay-x')}>
