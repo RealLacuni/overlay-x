f65fdc1b788c980dc47277fd3c15764262d2e108
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2024-01-04 18:28:40 -0500

    fix: concatenation bug with classnames

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -14,8 +14,8 @@ const SecondaryButton = ({ submit = false, className, onClick, children, disable
       type={submit ? 'submit' : 'button'}
       className={
         `${className}` +
-        `${disabled ? ' bg-gray-300 text-gray-400' : ' bg-indigo-100 hover:bg-indigo-200 text-indigo-700'}` +
-        'select-none undraggable inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded justify-center self-center align-middle  \
+        `${disabled ? ' bg-gray-300 text-gray-400' : ' bg-indigo-100 hover:bg-indigo-200 text-indigo-700'} ` +
+        'select-none inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs leading-4 font-medium rounded justify-center self-center align-middle  \
         focus:outline-none'
       }
       onClick={submit ? undefined : onClick}
@@ -32,8 +32,8 @@ const PrimaryButton = ({ submit = false, className, onClick, children, disabled 
       type={submit ? 'submit' : 'button'}
       className={
         `${className}` +
-        `${disabled ? ' bg-gray-300 text-gray-400' : ' bg-indigo-600 hover:bg-indigo-700'}` +
-        'undraggable inline-flex items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white \
+        `${disabled ? ' bg-gray-300 text-gray-400' : ' bg-indigo-600 hover:bg-indigo-700'} ` +
+        'select-none inline-flex items-center px-2.5 py-1.5 border border-transparent text-sm font-medium rounded shadow-sm text-white \
         focus:outline-none justify-center self-center align-middle'
       }
       {...(disabled ? { disabled: true } : {})}
@@ -49,8 +49,8 @@ const RoundButton = (props: Props) => {
     <button
       type="button"
       className={
-        'undraggable inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white \
-       bg-indigo-600 hover:bg-indigo-700 focus:outline-none' + `${props.className}`
+        'inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white \
+       bg-indigo-600 hover:bg-indigo-700 focus:outline-none ' + `${props.className}`
       }
       onClick={props.onClick}
     >
